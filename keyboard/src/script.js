4e4c2bd22767bdc8652d87f97235e61fff97a774
varenik40000 <64190191+varenik40000@users.noreply.github.com>
2023-04-26 21:36:51 +0500

    refactor: change render index page

--- keyboard/src/script.js
+++ keyboard/src/script.js
@@ -1,111 +1,104 @@
 'use strict';
-const div__title = document.createElement("div");
-div__title.classList.add('title');
-div__title.insertAdjacentHTML("beforeend", '<h1>RSS Виртуальная клавиатура</h1>');
-document.body.append(div__title);
+'use strict';
+function render() {
+  let body = document.body;
+  return body.innerHTML = `
+  <div class="title">
+  <h1>RSS Виртуальная клавиатура</h1>
+  </div>
+  <textarea class="textarea"></textarea>
+  <div class="wrapper">
+  <div class="line">
+  <div class="key" data-str="Backquote">\`</div>
+  <div class="key" data-str="Digit1">1</div>
+  <div class="key" data-str="Digit2">2</div>
+  <div class="key" data-str="Digit3">3</div>
+  <div class="key" data-str="Digit4">4</div>
+  <div class="key" data-str="Digit5">5</div>
+  <div class="key" data-str="Digit6">6</div>
+  <div class="key" data-str="Digit7">7</div>
+  <div class="key" data-str="Digit8">8</div>
+  <div class="key" data-str="Digit9">9</div>
+  <div class="key" data-str="Digit0">0</div>
+  <div class="key" data-str="Minus">-</div>
+  <div class="key" data-str="Equal">=</div>
+  <div class="fnkey bspace" data-str="Backspace">Backspace</div>
+  </div>
 
-const textarea = document.createElement("textarea");
-textarea.classList.add('textarea');
-textarea.insertAdjacentHTML("beforeend", '');
-document.body.append(textarea);
+  <div class="line">
+  <div class="fnkey tab" data-str="Tab">Tab</div>
+  <div class="key" data-str="KeyQ">q</div>
+  <div class="key" data-str="KeyW">w</div>
+  <div class="key" data-str="KeyE">e</div>
+  <div class="key" data-str="KeyR">r</div>
+  <div class="key" data-str="KeyT">t</div>
+  <div class="key" data-str="KeyY">y</div>
+  <div class="key" data-str="KeyU">u</div>
+  <div class="key" data-str="KeyI">i</div>
+  <div class="key" data-str="KeyO">o</div>
+  <div class="key" data-str="KeyP">p</div>
+  <div class="key" data-str="BracketLeft">[</div>
+  <div class="key" data-str="BracketRight">]</div>
+  <div class="key" data-str="Backslash">\\</div>
+  <div class="fnkey del"  data-str="Delete">Del</div>
+  </div>
 
-const div = document.createElement("div");
-div.classList.add('wrapper');
-document.body.append(div);
+  <div class="line">
+  <div class="fnkey capslock" data-str="CapsLock">CapsLock</div>
+  <div class="key" data-str="KeyA">a</div>
+  <div class="key" data-str="KeyS">s</div>
+  <div class="key" data-str="KeyD">d</div>
+  <div class="key" data-str="KeyF">f</div>
+  <div class="key" data-str="KeyG">g</div>
+  <div class="key" data-str="KeyH">h</div>
+  <div class="key" data-str="KeyJ">j</div>
+  <div class="key" data-str="KeyK">k</div>
+  <div class="key" data-str="KeyL">l</div>
+  <div class="key" data-str="Semicolon">;</div>
+  <div class="key" data-str="Quote">\'</div>
+  <div class="fnkey enter" data-str="Enter">Enter</div>
+  </div>
 
-const div__list1 = document.createElement("div");
-div__list1.classList.add('line');
-div__list1.insertAdjacentHTML("beforeend", '<div class="key" data="~">~</div>');
-div__list1.insertAdjacentHTML("beforeend", '<div class="key" data="1">1</div>');
-div__list1.insertAdjacentHTML("beforeend", '<div class="key" data="2">2</div>');
-div__list1.insertAdjacentHTML("beforeend", '<div class="key" data="3">3</div>');
-div__list1.insertAdjacentHTML("beforeend", '<div class="key" data="4">4</div>');
-div__list1.insertAdjacentHTML("beforeend", '<div class="key" data="5">5</div>');
-div__list1.insertAdjacentHTML("beforeend", '<div class="key" data="6">6</div>');
-div__list1.insertAdjacentHTML("beforeend", '<div class="key" data="7">7</div>');
-div__list1.insertAdjacentHTML("beforeend", '<div class="key" data="8">8</div>');
-div__list1.insertAdjacentHTML("beforeend", '<div class="key" data="9">9</div>');
-div__list1.insertAdjacentHTML("beforeend", '<div class="key" data="0">0</div>');
-div__list1.insertAdjacentHTML("beforeend", '<div class="key" data="-">-</div>');
-div__list1.insertAdjacentHTML("beforeend", '<div class="key" data="=">=</div>');
-div__list1.insertAdjacentHTML("beforeend", '<div class="fnkey bspace">Backspace</div>');
-div.appendChild(div__list1);
+  <div class="line">
+  <div class="fnkey shift" data-str="ShiftLeft">Shift</div>
+  <div class="key" data-str="KeyZ">z</div>
+  <div class="key" data-str="KeyX">x</div>
+  <div class="key" data-str="KeyC">c</div>
+  <div class="key" data-str="KeyV">v</div>
+  <div class="key" data-str="KeyB">b</div>
+  <div class="key" data-str="KeyN">n</div>
+  <div class="key" data-str="KeyM">m</div>
+  <div class="key" data-str="Comma">,</div>
+  <div class="key" data-str="Period">.</div>
+  <div class="key" data-str="Slash">/</div>
+  <div class="fnkey up" data-str="ArrowUp"></div>
+  <div class="fnkey shift" data-str="ShiftRight">Shift</div>
+  </div>
 
-const div__list2 = document.createElement("div");
-div__list2.classList.add('line');
-div__list2.insertAdjacentHTML("beforeend", '<div class="fnkey tab">Tab</div>');
-div__list2.insertAdjacentHTML("beforeend", '<div class="key" data="q">q</div>');
-div__list2.insertAdjacentHTML("beforeend", '<div class="key" data="w">w</div>');
-div__list2.insertAdjacentHTML("beforeend", '<div class="key" data="e">e</div>');
-div__list2.insertAdjacentHTML("beforeend", '<div class="key" data="r">r</div>');
-div__list2.insertAdjacentHTML("beforeend", '<div class="key" data="t">t</div>');
-div__list2.insertAdjacentHTML("beforeend", '<div class="key" data="y">y</div>');
-div__list2.insertAdjacentHTML("beforeend", '<div class="key" data="u">u</div>');
-div__list2.insertAdjacentHTML("beforeend", '<div class="key" data="i">i</div>');
-div__list2.insertAdjacentHTML("beforeend", '<div class="key" data="o">o</div>');
-div__list2.insertAdjacentHTML("beforeend", '<div class="key" data="p">p</div>');
-div__list2.insertAdjacentHTML("beforeend", '<div class="key" data="[">[</div>');
-div__list2.insertAdjacentHTML("beforeend", '<div class="key" data="]">]</div>');
-div__list2.insertAdjacentHTML("beforeend", '<div class="key" data="\\">\\</div>');
-div__list2.insertAdjacentHTML("beforeend", '<div class="fnkey del">Del</div>');
-div.appendChild(div__list2);
+  <div class="line">
+  <div class="fnkey Ctrl" data-str="ControlLeft">Ctrl</div>
+  <div class="fnkey Win" data-str="MetaLeft">Win</div>
+  <div class="fnkey Alt" data-str="AltLeft">Alt</div>
+  <div class="fnkey space" data-str="Space"></div>
+  <div class="fnkey Alt" data-str="AltRight">Alt</div>
+  <div class="fnkey left" data-str="ArrowLeft"></div>
+  <div class="fnkey left" data-str="ArrowDown"></div>
+  <div class="fnkey right" data-str="ArrowRight"></div>
+  <div class="fnkey Ctrl" data-str="ControlRight">Ctrl</div>
+  </div>
+  </div>
+`}
+render()
 
-const div__list3 = document.createElement("div");
-div__list3.classList.add('line');
-div__list3.insertAdjacentHTML("beforeend", '<div class="fnkey capslock">Caps Lock</div>');
-div__list3.insertAdjacentHTML("beforeend", '<div class="key" data="a">a</div>');
-div__list3.insertAdjacentHTML("beforeend", '<div class="key" data="s">s</div>');
-div__list3.insertAdjacentHTML("beforeend", '<div class="key" data="d">d</div>');
-div__list3.insertAdjacentHTML("beforeend", '<div class="key" data="f">f</div>');
-div__list3.insertAdjacentHTML("beforeend", '<div class="key" data="g">g</div>');
-div__list3.insertAdjacentHTML("beforeend", '<div class="key" data="h">h</div>');
-div__list3.insertAdjacentHTML("beforeend", '<div class="key" data="j">j</div>');
-div__list3.insertAdjacentHTML("beforeend", '<div class="key" data="k">k</div>');
-div__list3.insertAdjacentHTML("beforeend", '<div class="key" data="l">l</div>');
-div__list3.insertAdjacentHTML("beforeend", '<div class="key" data=";">;</div>');
-div__list3.insertAdjacentHTML("beforeend", '<div class="key" data="\'">\'</div>');
-div__list3.insertAdjacentHTML("beforeend", '<div class="enter" data="<br>">Enter</div>');
-div.appendChild(div__list3);
-
-const div__list4 = document.createElement("div");
-div__list4.classList.add('line');
-div__list4.insertAdjacentHTML("beforeend", '<div class="fnkey shift">Shift</div>');
-div__list4.insertAdjacentHTML("beforeend", '<div class="key" data="z">z</div>');
-div__list4.insertAdjacentHTML("beforeend", '<div class="key" data="x">x</div>');
-div__list4.insertAdjacentHTML("beforeend", '<div class="key" data="c">c</div>');
-div__list4.insertAdjacentHTML("beforeend", '<div class="key" data="v">v</div>');
-div__list4.insertAdjacentHTML("beforeend", '<div class="key" data="b">b</div>');
-div__list4.insertAdjacentHTML("beforeend", '<div class="key" data="n">n</div>');
-div__list4.insertAdjacentHTML("beforeend", '<div class="key" data="m">m</div>');
-div__list4.insertAdjacentHTML("beforeend", '<div class="key" data=",">,</div>');
-div__list4.insertAdjacentHTML("beforeend", '<div class="key" data=".">.</div>');
-div__list4.insertAdjacentHTML("beforeend", '<div class="key" data="/">/</div>');
-div__list4.insertAdjacentHTML("beforeend", '<div class="fnkey up"></div>');
-div__list4.insertAdjacentHTML("beforeend", '<div class="fnkey shift">Shift</div>');
-div.appendChild(div__list4);
-
-const div__list5 = document.createElement("div");
-div__list5.classList.add('line');
-div__list5.insertAdjacentHTML("beforeend", '<div class="fnkey Ctrl">Ctrl</div>');
-div__list5.insertAdjacentHTML("beforeend", '<div class="fnkey Win">Win</div>');
-div__list5.insertAdjacentHTML("beforeend", '<div class="fnkey Alt">Alt</div>');
-div__list5.insertAdjacentHTML("beforeend", '<div class="fnkey space"></div>');
-div__list5.insertAdjacentHTML("beforeend", '<div class="fnkey Alt">Alt</div>');
-div__list5.insertAdjacentHTML("beforeend", '<div class="fnkey left"></div>');
-div__list5.insertAdjacentHTML("beforeend", '<div class="fnkey down"></div>');
-div__list5.insertAdjacentHTML("beforeend", '<div class="fnkey right"></div>');
-div__list5.insertAdjacentHTML("beforeend", '<div class="fnkey Ctrl">Ctrl</div>');
-div.appendChild(div__list5);
-
-
-
-
+function print() {
 let key = document.querySelectorAll('.key');
 let display = document.querySelector('.textarea');
 
-for(let k of key){
-k.onclick = function(){
+for (let k of key) {
+  k.onclick = function () {
     display.textContent += k.textContent;
   }
-}  
+}
+}
+print()
 
